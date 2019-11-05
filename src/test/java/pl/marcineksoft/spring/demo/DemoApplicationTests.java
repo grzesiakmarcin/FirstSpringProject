b34d4d9b2bea5e07c2ac638315b7e799a79a82ab
Marcin Grzesiak <mk.grzesiak@gmail.com>
2019-11-05 19:20:35 +0100

    NewRepo

---
+++ src/test/java/pl/marcineksoft/spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcineksoft.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
